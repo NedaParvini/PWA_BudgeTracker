5385f4ebb7e0ff909c8bec1eac76c8f4737a4214
NedaParvini <nedaparvini1@gmail.com>
2022-02-16 09:54:45 -0800

    idb modified

--- public/js/idb.js
+++ public/js/idb.js
@@ -7,58 +7,58 @@ request.onupgradeneeded = function(event) {
   db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 
-// request.onsuccess = function(event) {
-//   db = event.target.result;
+request.onsuccess = function(event) {
+  db = event.target.result;
 
-//   if (navigator.onLine) {
-//     uploadTransactions();
-//   }
-// };
-
-// request.onerror = function(event) {
-//   console.log(event.target.errorCode);
-// };
-
-function saveRecord(record) {
-  const transaction = db.transaction(['new_transaction'], 'readwrite');
-
-  const transactionObjectStore = transaction.objectStore('new_transaction');
-
-  transactionObjectStore.add(record);
-}
-
-function uploadTransactions() {
-  const transaction = db.transaction(['new_transaction'], 'readwrite');
-
-  const transactionObjectStore = transaction.objectStore('new_transaction');
-
-  const getAll = transactionObjectStore.getAll();
-
-  getAll.onsuccess = function() {
-    if (getAll.result.length > 0) {
-      fetch('/api/transaction', {
-        method: 'POST',
-        body: JSON.stringify(getAll.result),
-        headers: {
-          Accept: 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(response => response.json())
-      .then(serverResponse => {
-        if (serverResponse.message) {
-          throw new Error(serverResponse);
-        }
-        const transaction = db.transaction(['new_transaction'], 'readwrite');
-        const transactionObjectStore = transaction.objectStore('new_transaction');
-        transactionObjectStore.clear();
-        alert('Offline transactions have been submitted');
-      })
-      .catch(err => {
-        console.log(err);
-      });
-    }
-  };
+  if (navigator.onLine) {
+    uploadTransactions();
+  }
 };
+
+request.onerror = function(event) {
+  console.log(event.target.errorCode);
+};
+
+// function saveRecord(record) {
+//   const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+//   const transactionObjectStore = transaction.objectStore('new_transaction');
+
+//   transactionObjectStore.add(record);
+// }
+
+// function uploadTransactions() {
+//   const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+//   const transactionObjectStore = transaction.objectStore('new_transaction');
+
+//   const getAll = transactionObjectStore.getAll();
+
+//   getAll.onsuccess = function() {
+//     if (getAll.result.length > 0) {
+//       fetch('/api/transaction', {
+//         method: 'POST',
+//         body: JSON.stringify(getAll.result),
+//         headers: {
+//           Accept: 'application/json, text/plain, */*',
+//           'Content-Type': 'application/json'
+//         }
+//       })
+//       .then(response => response.json())
+//       .then(serverResponse => {
+//         if (serverResponse.message) {
+//           throw new Error(serverResponse);
+//         }
+//         const transaction = db.transaction(['new_transaction'], 'readwrite');
+//         const transactionObjectStore = transaction.objectStore('new_transaction');
+//         transactionObjectStore.clear();
+//         alert('Offline transactions have been submitted');
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//     }
+//   };
+// };
 
 window.addEventListener('online', uploadTransactions);
