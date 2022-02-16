624114d6e81259aefc268a32a8814b150ce1e8fc
NedaParvini <nedaparvini1@gmail.com>
2022-02-15 18:00:29 -0800

    service worker has been changed2

--- public/service-worker.js
+++ public/service-worker.js
@@ -21,31 +21,31 @@ const FILES_TO_CACHE = [
   
 ];
 
-// self.addEventListener('install', function(e) {
-//   e.waitUntil(
-//     caches.open(CACHE_NAME).then(function (cache) {
-//       console.log('installing cache : ' + CACHE_NAME)
-//       return cache.addAll(FILES_TO_CACHE)
-//     })
-//   )
-//   self.skipWaiting();
-// });
-
-self.addEventListener('activate', function(e) {
+self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log('Removing old cache data', key);
-            return caches.delete(key);
-          }
-        })
-      );
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(FILES_TO_CACHE)
     })
-  );
-  self.clients.claim();
+  )
+  self.skipWaiting();
 });
+
+// self.addEventListener('activate', function(e) {
+//   e.waitUntil(
+//     caches.keys().then(keyList => {
+//       return Promise.all(
+//         keyList.map(key => {
+//           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+//             console.log('Removing old cache data', key);
+//             return caches.delete(key);
+//           }
+//         })
+//       );
+//     })
+//   );
+//   self.clients.claim();
+// });
 
 self.addEventListener('fetch', function(e) {
   if (e.request.url.includes('/api/')) {
