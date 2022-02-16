7f90a99aa24c577715464613bc868f2c4897e4c5
NedaParvini <nedaparvini1@gmail.com>
2022-02-15 17:58:53 -0800

    service worker has been changed

--- public/service-worker.js
+++ public/service-worker.js
@@ -21,15 +21,15 @@ const FILES_TO_CACHE = [
   
 ];
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(FILES_TO_CACHE)
-    })
-  )
-  self.skipWaiting();
-});
+// self.addEventListener('install', function(e) {
+//   e.waitUntil(
+//     caches.open(CACHE_NAME).then(function (cache) {
+//       console.log('installing cache : ' + CACHE_NAME)
+//       return cache.addAll(FILES_TO_CACHE)
+//     })
+//   )
+//   self.skipWaiting();
+// });
 
 self.addEventListener('activate', function(e) {
   e.waitUntil(
